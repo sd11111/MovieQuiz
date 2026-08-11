a6fb57d8b010505c80ef67f3ada10b988fea7ec7
Semen <semendav@icloud.com>
2026-08-11 23:48:19 +0300

    Добавлены индикаторы загрузки. Проведена работа с загрузкой данных, теперь названия фильмов, картинки и рейтинг загружаются через сеть.

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_j4r66gt6") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(
+            url: mostPopularMoviesUrl,
+            handler: { result in
+                switch result {
+                case .success(let data):
+                    let decoder = JSONDecoder()
+                    do {
+                        let movies = try decoder.decode(MostPopularMovies.self, from: data)
+                        handler(.success(movies))
+                    } catch {
+                        handler(.failure(error))
+                    }
+                case .failure(let error):
+                    handler(.failure(error))
+                }
+            }
+        )
+        
+    }
+}
+
+
